45bae70625dfa0d4a5a6bfb3dcb6866d16be9ded
GM <meszaros275@gmail.com>
2019-08-17 23:14:40 +0100

    Simple Krypton mining pool UI

---
+++ config.js
@@ -0,0 +1,14 @@
+var api = "http://127.0.0.1:8117";
+
+var email = "";
+var telegram = "https://t.me/kryptonchain";
+var discord = "";
+var facebook = "";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://explorer.kryptonchain.org/?hash={id}#blockchain_block";
+var transactionExplorer = "https://explorer.kryptonchain.org/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
